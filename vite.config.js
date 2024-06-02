c5a7ffe5e68784dcb8431116954b57b18a13465f
sriramgarimella <sriram.garimella@gmail.com>
2024-06-02 15:46:34 +0530

    fixed generated js to constant name in vite config export

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+        output: {
+            dir: 'dist/spin/',
+            entryFileNames: 'application-entry.js',
+            assetFileNames: 'application-asset.css',
+            chunkFileNames: "application-chunk.js",
+            manualChunks: undefined,
+        }
+    }
+}
 })
